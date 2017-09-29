1f3d98c5efc670acc1749179c38e09aabc8b4d33
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-09-29 11:54:33 -0700

    Init commit

---
+++ src/WebComponent.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import defineWebComponent from 'metal.js/packages/metal-web-component/lib/define_web_component';
+import templates from './WebComponent.soy.js';
+
+import './web-component.scss';
+
+class WebComponent extends Component {
+}
+Soy.register(WebComponent, templates);
+
+defineWebComponent('web-component', WebComponent);
+
+export { WebComponent };
+export default WebComponent;
